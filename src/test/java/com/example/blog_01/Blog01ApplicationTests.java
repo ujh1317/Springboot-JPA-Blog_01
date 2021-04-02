021948add4f27dc5fea46892e87d4b5d713ba671
Yujeonghyeon <dbwjdgus1317@gmail.com>
2021-04-02 19:15:48 +0900

    first commit

---
+++ src/test/java/com/example/blog_01/Blog01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
